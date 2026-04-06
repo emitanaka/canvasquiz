00fcb324cc8dd0c9e599fef2ed7af67cb3a0d3a1
Emi Tanaka <dr.emi.tanaka@gmail.com>
2026-04-07 09:39:13 +1000

    submission file

--- R/grade.R
+++ R/grade.R
@@ -1,18 +1,19 @@
 
-#' Get quiz submission details
-#' 
-#' @param submission_id The ID of the quiz submission to retrieve details for.
-#' @inheritParams quiz_questions
-#' @export
-submission_info <- function(submission_id, 
-                            course_id = Sys.getenv("CANVASQUIZ_COURSE_ID"), 
-                            url = Sys.getenv("CANVASQUIZ_URL"), 
-                            token = Sys.getenv("CANVASQUIZ_TOKEN")) {
-  canvas_url(url, token) |>
-    httr2::req_url_path_append(paste0("quiz_submissions/", submission_id, "/questions")) |>
-    httr2::req_perform() |> 
-    httr2::resp_body_json() |> 
-    pluck(1)
+
+list_attempted_questions <- function(quiz_id, 
+                                     course_id = Sys.getenv("CANVASQUIZ_COURSE_ID"), 
+                                     url = Sys.getenv("CANVASQUIZ_URL"), 
+                                     token = Sys.getenv("CANVASQUIZ_TOKEN")) {
+  submissions <- list_submissions(quiz_id, course_id, url, token)
+  lapply(submissions$id, function(id) {
+    submission_questions(submission_id = id, url, token)
+  }) |>
+    dplyr::bind_rows() |> 
+    dplyr::summarise(submission_id = list(submission_id),
+                     n = dplyr::n(),
+                     .by = c(question_id, quiz_id, position, question_name, question_type, question_text)) |> 
+    dplyr::arrange(position)
+  
 }
 
 
@@ -48,124 +49,3 @@ submission_info <- function(submission_id,
 #                attempt = vapply(res$submissions, function(x) x$attempt, numeric(1)))
 #   }
 # }
-
-
-#' List quiz submissions
-#' 
-#' @inheritParams quiz_results
-#' @export
-list_submissions <- function(quiz_id, 
-                             course_id = Sys.getenv("CANVASQUIZ_COURSE_ID"), 
-                             url = Sys.getenv("CANVASQUIZ_URL"), 
-                             token = Sys.getenv("CANVASQUIZ_TOKEN"),
-                             n = count_submissions(quiz_id, course_id, url, token),
-                             tz = Sys.timezone()) {
-  get_submissions_page <- function(page) {
-    quizzes_url(course_id) |>
-      httr2::req_url_path_append(paste0(quiz_id, "/submissions")) |>
-      httr2::req_url_query(per_page = 100, page = page) |>
-      httr2::req_perform() |>
-      httr2::resp_body_json() |> 
-      dplyr::bind_rows()
-  }
-  resp <- resp1 <- get_submissions_page(ipage <- 1)
-  while (nrow(resp1) == 100 & nrow(resp) < n) {
-    resp1 <- get_submissions_page(ipage <- ipage + 1)
-    resp <- dplyr::bind_rows(resp, resp1)
-  }
-  out <- resp |> 
-    dplyr::mutate(across(c(started_at, finished_at, end_at), \(x) as.POSIXct(x, format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")))
-  attr(out$started_at, "tzone") <- tz
-  attr(out$finished_at, "tzone") <- tz
-  attr(out$end_at, "tzone") <- tz
-  out
-}
-
-list_attempted_questions <- function(quiz_id, 
-                                     course_id = Sys.getenv("CANVASQUIZ_COURSE_ID"), 
-                                     url = Sys.getenv("CANVASQUIZ_URL"), 
-                                     token = Sys.getenv("CANVASQUIZ_TOKEN")) {
-  submissions <- list_submissions(quiz_id, course_id, url, token)
-  lapply(submissions$id, function(id) {
-    submission_questions(submission_id = id, course_id, url, token)
-  }) |>
-    dplyr::bind_rows() |> 
-    dplyr::count(question_id, quiz_id, position, question_name, question_type, question_text) |> 
-    dplyr::arrange(position)
-  
-}
-
-submission_questions <- function(submission_id, 
-                                 url = Sys.getenv("CANVASQUIZ_URL"), 
-                                 token = Sys.getenv("CANVASQUIZ_TOKEN")) {
-  res <- canvas_url(url, token) |>
-    httr2::req_url_path_append(paste0("quiz_submissions/", submission_id, "/questions")) |>
-    httr2::req_perform() |> 
-    httr2::resp_body_json() |> 
-    pluck(1)
-  tibble::tibble(
-    question_id = vapply(res, function(x) x$id, numeric(1)),
-    quiz_id = vapply(res, function(x) x$quiz_id, numeric(1)),
-    position = vapply(res, function(x) x$position, numeric(1)),
-    question_name = vapply(res, function(x) x$question_name, character(1)),
-    question_type = vapply(res, function(x) x$question_type, character(1)),
-    question_text = vapply(res, function(x) x$question_text, character(1))
-  ) |> 
-    dplyr::arrange(position)
-}
-
-# question_info(question_id, quiz_id, course_id, url, token)
-
-
-submission_info2 <- function(submission_id, 
-                             course_id = Sys.getenv("CANVASQUIZ_COURSE_ID"), 
-                             url = Sys.getenv("CANVASQUIZ_URL"), 
-                             token = Sys.getenv("CANVASQUIZ_TOKEN")) {
-  quiz_id <- submission_info(submission_id, course_id, url, token) |>
-    pluck(1) |> 
-    pluck("quiz_id")
-  res <- quizzes_url(course_id, url, token) |> 
-        httr2::req_url_path_append(paste0(quiz_id, "/submissions/", submission_id)) |>
-        httr2::req_body_json(list(include = c("user", "quiz", "submission"))) |> 
-        httr2::req_method("GET") |> 
-        httr2::req_perform() |> 
-        httr2::resp_body_json() 
-  name <- res$users[[1]]$name
-  quiz <- res$quizzes[[1]]$title
-  score <- res$submissions[[1]]$score
-  attempt <- res$submissions[[1]]$attempt
-  list(
-    name = name,
-    quiz = quiz,
-    score = score,
-    attempt = attempt,
-    quiz_id = quiz_id
-  )
-}
-
-
-submission_update <- function(submission_id, 
-                              fudge_points = NULL,
-                              question_id = NULL,
-                              score = NULL,
-                              comment = NULL,
-                              course_id = Sys.getenv("CANVASQUIZ_COURSE_ID"), 
-                              url = Sys.getenv("CANVASQUIZ_URL"), 
-                              token = Sys.getenv("CANVASQUIZ_TOKEN")) {
-  
-  details <- submission_info2(submission_id, course_id, url, token)
-  stopifnot(length(details) > 0)
-  quiz_id <- details$quiz_id
-  update_list <- setNames(list(list(score = score, comment = comment)), question_id)
-  cli::cli_inform("Updating submission {.val {submission_id}} for {.val {details$name}} on quiz {.val {details$quiz}} (attempt {.val {details$attempt}}, current score: {.val {details$score}}).")
-  
-  quizzes_url(course_id, url, token) |>
-    httr2::req_url_path_append(paste0(quiz_id, "/submissions/", submission_id)) |>
-    httr2::req_body_json(list(quiz_submissions = list(list(attempt = details$attempt, 
-                                                           fudge_points = fudge_points, 
-                                                           questions = update_list)))) |> 
-    httr2::req_method("PUT") |> 
-    #httr2::req_dry_run()
-    httr2::req_perform() |> 
-    httr2::resp_body_json() 
-}
---
+++ R/submission.R
@@ -0,0 +1,151 @@
+
+#' Get quiz submission details
+#' 
+#' @param submission_id The ID of the quiz submission to retrieve details for.
+#' @inheritParams quiz_questions
+#' @family submissions
+#' @export
+submission_info <- function(submission_id, 
+                            course_id = Sys.getenv("CANVASQUIZ_COURSE_ID"), 
+                            url = Sys.getenv("CANVASQUIZ_URL"), 
+                            token = Sys.getenv("CANVASQUIZ_TOKEN")) {
+  canvas_url(url, token) |>
+    httr2::req_url_path_append(paste0("quiz_submissions/", submission_id, "/questions")) |>
+    httr2::req_perform() |> 
+    httr2::resp_body_json() |> 
+    pluck(1) |> 
+    lapply(function(x) {
+      x$correct <- as.character(x$correct)
+      x
+    }) |> 
+    dplyr::bind_rows() |> 
+    dplyr::arrange(position) |> 
+    dplyr::summarise(
+      answers = list(answers),
+      .by = c(id, quiz_id, position, question_name, question_type, question_text, flagged, correct, assessment_question_id, quiz_group_id)
+    )
+}
+
+#' @rdname submission_info
+#' @export
+submission_overview <- function(submission_id, 
+                                course_id = Sys.getenv("CANVASQUIZ_COURSE_ID"), 
+                                url = Sys.getenv("CANVASQUIZ_URL"), 
+                                token = Sys.getenv("CANVASQUIZ_TOKEN")) {
+  quiz_id <- submission_info(submission_id, course_id, url, token) |>
+    pluck("quiz_id") |> 
+    pluck(1)
+  res <- quizzes_url(course_id, url, token) |> 
+        httr2::req_url_path_append(paste0(quiz_id, "/submissions/", submission_id)) |>
+        httr2::req_body_json(list(include = c("user", "quiz", "submission"))) |> 
+        httr2::req_method("GET") |> 
+        httr2::req_perform() |> 
+        httr2::resp_body_json() 
+  tibble::tibble(
+    name = res$users[[1]]$name,
+    quiz = res$quizzes[[1]]$title,
+    score = res$submissions[[1]]$score,
+    attempt = res$submissions[[1]]$attempt,
+    quiz_id = quiz_id,
+    submission_id = submission_id
+  )
+}
+
+
+#' List quiz submissions
+#' 
+#' @inheritParams quiz_results
+#' @family submissions
+#' @export
+list_submissions <- function(quiz_id, 
+                             course_id = Sys.getenv("CANVASQUIZ_COURSE_ID"), 
+                             url = Sys.getenv("CANVASQUIZ_URL"), 
+                             token = Sys.getenv("CANVASQUIZ_TOKEN"),
+                             n = count_submissions(quiz_id, course_id, url, token),
+                             tz = Sys.timezone()) {
+  get_submissions_page <- function(page) {
+    quizzes_url(course_id) |>
+      httr2::req_url_path_append(paste0(quiz_id, "/submissions")) |>
+      httr2::req_url_query(per_page = 100, page = page) |>
+      httr2::req_perform() |>
+      httr2::resp_body_json() |> 
+      dplyr::bind_rows()
+  }
+  resp <- resp1 <- get_submissions_page(ipage <- 1)
+  while (nrow(resp1) == 100 & nrow(resp) < n) {
+    resp1 <- get_submissions_page(ipage <- ipage + 1)
+    resp <- dplyr::bind_rows(resp, resp1)
+  }
+  out <- resp |> 
+    dplyr::mutate(across(c(started_at, finished_at, end_at), \(x) as.POSIXct(x, format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")))
+  attr(out$started_at, "tzone") <- tz
+  attr(out$finished_at, "tzone") <- tz
+  attr(out$end_at, "tzone") <- tz
+  out
+}
+
+#' List attempted questions for a quiz
+#' 
+#' @inheritParams submission_info
+#' @family submissions
+#' @export
+submission_questions <- function(submission_id, 
+                                 url = Sys.getenv("CANVASQUIZ_URL"), 
+                                 token = Sys.getenv("CANVASQUIZ_TOKEN")) {
+  stopifnot(length(submission_id) == 1)
+  res <- canvas_url(url, token) |>
+    httr2::req_url_path_append(paste0("quiz_submissions/", submission_id, "/questions")) |>
+    httr2::req_perform() |> 
+    httr2::resp_body_json() |> 
+    pluck(1)
+  tibble::tibble(
+    question_id = vapply(res, function(x) x$id, numeric(1)),
+    quiz_id = vapply(res, function(x) x$quiz_id, numeric(1)),
+    position = vapply(res, function(x) x$position, numeric(1)),
+    question_name = vapply(res, function(x) x$question_name, character(1)),
+    question_type = vapply(res, function(x) x$question_type, character(1)),
+    question_text = vapply(res, function(x) x$question_text, character(1))
+  ) |> 
+    dplyr::arrange(position) |> 
+    dplyr::mutate(submission_id = submission_id)
+}
+
+
+#' Update a quiz submission
+#' 
+#' @inheritParams submission_info
+#' 
+#' @export
+submission_update <- function(submission_id, 
+                              fudge_points = NULL,
+                              question_id = NULL,
+                              score = NULL,
+                              comment = NULL,
+                              course_id = Sys.getenv("CANVASQUIZ_COURSE_ID"), 
+                              url = Sys.getenv("CANVASQUIZ_URL"), 
+                              token = Sys.getenv("CANVASQUIZ_TOKEN")) {
+  stopifnot(length(submission_id) == 1,
+            is.null(fudge_points) || length(fudge_points) == 1,
+            is.null(question_id) || length(question_id) == 1,
+            is.null(score) || length(score) == 1,
+            is.null(comment) || length(comment) == 1)
+  
+  details <- submission_overview(submission_id, course_id, url, token)
+  stopifnot(length(details) > 0)
+  quiz_id <- details$quiz_id
+  if(is.null(question_id)) {
+    update_list <- list()
+  } else {
+    update_list <- setNames(list(list(score = score, comment = comment)), question_id)
+  }
+  cli::cli_inform("Updating submission {.val {submission_id}} for {.val {details$name}} on quiz {.val {details$quiz}} (attempt {.val {details$attempt}}, current score: {.val {details$score}}).")
+  
+  quizzes_url(course_id, url, token) |>
+    httr2::req_url_path_append(paste0(quiz_id, "/submissions/", submission_id)) |>
+    httr2::req_body_json(list(quiz_submissions = list(list(attempt = details$attempt, 
+                                                           fudge_points = fudge_points, 
+                                                           questions = update_list)))) |> 
+    httr2::req_method("PUT") |> 
+    httr2::req_perform() |> 
+    httr2::resp_body_json() 
+}
